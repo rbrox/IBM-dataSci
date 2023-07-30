42fd81d75cb116c9f3008274b54e74b6fc5ce96b
rbrox <rishavsci@gmail.com>
2023-07-30 13:36:24 +0530

    added

--- ProfitProMax/model_training.py
+++ ProfitProMax/model_training.py
@@ -1,47 +1,62 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
-def select_model():
+def select_model(model_type='random_forest'):
     """
-    Select the appropriate forecasting model.
-    
-    Returns:
-        model: The selected forecasting model.
-    """
-    # Implement logic to select the appropriate model
-    return RandomForestRegressor()
-
-def hyperparameter_tuning(model):
-    """
-    Perform hyperparameter tuning of the selected model.
+    Select the appropriate regression model.
     
     Args:
-        model: The selected forecasting model.
+        model_type (str): Type of regression model, either 'linear' or 'random_forest'.
+    
+    Returns:
+        model: The selected regression model.
+    """
+    if model_type == 'linear':
+        return LinearRegression()
+    elif model_type == 'random_forest':
+        return RandomForestRegressor()
+    else:
+        raise ValueError("Invalid model_type. Supported options are 'linear' and 'random_forest'.")
+
+def hyperparameter_tuning(model, param_grid):
+    """
+    Perform hyperparameter tuning of the selected model using Grid Search.
+    
+    Args:
+        model: The selected regression model.
+        param_grid (dict): Dictionary containing hyperparameter values for Grid Search.
         
     Returns:
-        model: The tuned forecasting model.
+        model: The tuned regression model.
     """
-    # Implement logic for hyperparameter tuning
-    return model
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
+    grid_search.fit(X_train, y_train)
+    return grid_search.best_estimator_
 
-def train_model(data):
+def train_model(data, model_type='random_forest', param_grid=None):
     """
-    Train the sales forecasting model on the provided data.
+    Train the regression model on the provided data.
     
     Args:
         data (pd.DataFrame): The input DataFrame containing the training data.
+        model_type (str): Type of regression model, either 'linear' or 'random_forest'.
+        param_grid (dict, optional): Dictionary containing hyperparameter values for Grid Search.
         
     Returns:
-        model: The trained sales forecasting model.
+        model: The trained regression model.
     """
-    X = data.drop('sales', axis=1)
-    y = data['sales']
+    X = data.drop('target', axis=1)
+    y = data['target']
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    model = select_model()
-    tuned_model = hyperparameter_tuning(model)
-    tuned_model.fit(X_train, y_train)
+    model = select_model(model_type=model_type)
     
-    return tuned_model
+    if param_grid:
+        model = hyperparameter_tuning(model, param_grid)
+    
+    model.fit(X_train, y_train)
+    
+    return model
